8dac47f1d7e318554b09e6556d27bbd7c7180bb9
ironlung13 <ironlung13@mail.ru>
2021-02-25 20:36:25 +0300

    Add project files.

---
+++ CustomFormulae.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1_Newton_s_Method
+{
+    public static class CustomFormulae
+    {
+        public static double FindNthRootOfNumber(double number, uint power, double accuracy)
+        {
+            if ((number < 0 && power % 2 == 0) ||
+                power == 0 ||
+                double.IsNaN(number) ||
+                double.IsNegativeInfinity(number) ||
+                double.IsPositiveInfinity(number))
+            {
+                throw new ArgumentException("number and/or degree is invalid.");
+            }
+
+            if (accuracy < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(accuracy));
+            }
+
+            if (power == 1)
+            {
+                return number;
+            }
+            else
+            {
+                double result;
+                double resultPrevious = number; //first assumption
+                while (true)
+                {
+                    result = 1d / power * ((resultPrevious * (power - 1)) + (number / Math.Pow(resultPrevious, power - 1)));
+                    if (Math.Abs(result - resultPrevious) < accuracy)
+                    {
+                        return result;
+                    }
+                    else
+                    {
+                        resultPrevious = result;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Task1_Newton_s_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        EntryPoint:
+            Console.WriteLine("Welcome, User!");
+            Console.Write("Enter number in question (floating point number):\n=> ");
+
+
+            double number;
+            while (!double.TryParse(Console.ReadLine(), out number) ||
+                    number > double.MaxValue ||
+                    number < double.MinValue)
+            {
+                Console.Write("Invalid input. Try again\n=> ");
+            }
+
+
+            uint power;
+            Console.Write("Enter power of root value(positive non-zero integer):\n=> ");
+
+        EnteringPowerOfRoot:
+            while (!uint.TryParse(Console.ReadLine(), out power) ||
+                    power == 0)
+            {
+                Console.Write("Invalid input. Try again\n=> ");
+            }
+
+            if (number < 0 && power % 2 == 0)
+            {
+                Console.WriteLine($"{number} is less than zero, so power of root can't be even.");
+                goto EnteringPowerOfRoot;
+            }
+
+            double precision;
+            Console.WriteLine("Enter precision value (the lower, the better).");
+            Console.Write("For epsilon enter \"epsilon\"\n=> ");
+
+        EnteringPrecision:
+            string answer = Console.ReadLine();
+            switch (answer)
+            {
+                case "epsilon":
+                    precision = double.Epsilon;
+                    break;
+                default:
+                    if (!double.TryParse(answer, out precision))
+                    {
+                        Console.Write("Invalid input. Try again\n=> ");
+                        goto EnteringPrecision;
+                    }
+                    else
+                        break;
+            }
+
+            Console.WriteLine($"\nNumber=> {number}");
+            Console.WriteLine($"Power of root=> {power}");
+            double result = CustomFormulae.FindNthRootOfNumber(number, power, precision);
+            Console.WriteLine($"Approximate Root: {result}");
+            Console.WriteLine($"Root calculated via Math.Pow=>{Math.Pow(number,1d / power)}");
+
+            Console.WriteLine("To quit, enter \"q\"");
+            Console.WriteLine("To restart program, enter anything else.");
+            switch(Console.ReadLine())
+            {
+                case "q":
+                    return;
+                default:
+                    Console.Clear();
+                    goto EntryPoint;
+            }
+        }
+    }
+}
